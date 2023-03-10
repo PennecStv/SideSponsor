2e1b0721a3e2829e49a9de81e36e168c3b4d0340
Steve PENNEC <steve.pennec@etu.univ-lyon1.fr>
2023-03-10 04:08:10 +0100

    Interface created

--- src/Controller/ContractController.php
+++ src/Controller/ContractController.php
@@ -11,7 +11,24 @@ class ContractController extends AbstractController
     #[Route('/contract', name: 'app_contract')]
     public function index(): Response
     {
+        $contract_list = array(
+            "nom" => "Steve",
+            "nom_mecene" => "Apple",
+            "date_debut" => "13/02/2003",
+            "date_fin"   => "31/03/2023"
+        );
+
         return $this->render('contract/index.html.twig', [
+            'controller_name' => 'ContractController',
+            "contract_list"   => $contract_list
+        ]);
+    }
+
+    #[Route('/addContract', name: 'add_contract')]
+    public function addContract() : Response {
+
+
+        return $this->render('contract/add.html.twig', [
             'controller_name' => 'ContractController',
         ]);
     }
